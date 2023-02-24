88341594816abf61748c024e65dbe35e776b55cf
Ata <atadurweshziyaee@gmail.com>
2023-02-24 23:39:52 +0530

    Write a java prog to create a array and pthe the element.

---
+++ CreatArray.java
@@ -0,0 +1,9 @@
+// Write a java program to create a array and print the element.
+import java.util.Arrays;
+public class CreatArray{
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5,6,33,7,8,2}; // declaring the array and initializing ;
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
